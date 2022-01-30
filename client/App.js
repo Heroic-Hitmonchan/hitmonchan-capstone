d0a81f2097fd10dfe8bea11a3e24576ef1ff0967
Rafet Abdalgalil <rafetalani@gmail.com>
2022-01-29 21:16:27 -0700

    added home component and still working on the routes

--- client/App.js
+++ client/App.js
@@ -1,73 +1,15 @@
-// import {CLIENT_ID, CLIENT_SECRET} from '@env'
-import * as React from 'react'
-import { StatusBar } from 'expo-status-bar';
-import PhotoScreen from './components/PhotoScreen'
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useAuthRequest, ResponseType } from 'expo-auth-session';
-import { Provider, useDispatch, useSelector } from "react-redux";
-import Routes from './components/Routers'
-import { setAuthorization } from './redux/auth'
-import {store} from './index';
+import { Provider } from "react-redux";
+import { store } from './index';
+import Routes from "./components/Routers";
 
-const discovery = {
-  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
-  tokenEndpoint: 'https://accounts.spotify.com/api/token'
-}
 
-export default function AppWrapper () {
 
-  return (
-    <Provider store={store}> 
-      <App />
-    </Provider>
-  )
-}
 
- const App = () => {
-  // console.log(CLIENT_ID)
-  // console.log(CLIENT_SECRET)
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      responseType: ResponseType.Token,
-      clientId: '12ab9fc82d684679b569135ea050d5d8',
-      clientSecret: '3c13d792c46d4c33a76bd6b2e8b5de94',
-      scopes: ['user-read-email', 'playlist-modify-public'],
-      usePKCE: false,
-      redirectUri: 'exp://172.25.221.63:19000'
-    },
-    discovery
-  )
+export default function App() {
 
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    if (response?.type === 'success') {
-      const { access_token } = response.params
-      console.log("Access Token", access_token)
-      dispatch(setAuthorization(access_token));
-    }
-  }, [response])
-
-  return (
-    // <Routes />
-    
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-        <Button disabled={!request} title="Login" onPress={() => promptAsync()} />
-        <StatusBar style="auto" />
-        < PhotoScreen />
-      </View>
-   
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 40
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-});
-
+    return (
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    )
+  }
--- client/components/Routers.js
+++ client/components/Routers.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { Router, Scene } from 'react-native-router-flux';
+import { Router, Scene, Stack } from 'react-native-router-flux';
+import { useDispatch, useSelector } from "react-redux";
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Home from './Home';
 import PhotoScreen from './PhotoScreen'
 
 const Routes = () => {
 
-    return (
-        <View>
+    const isLoggedIn = useSelector((state) => {
+        return !!state.auth;
+    });
 
-        </View>
+    const dispatch = useDispatch();
+
+    return (
+        <Router>
+            {isLoggedIn ? (
+                <Scene key='root'>
+                    <Scene key='Home' component={Home} initial={true} />
+                    <Scene key='PhotoScreen' component={PhotoScreen} title={PhotoScreen}/>
+                </Scene>
+            ) : (
+                <Scene key='root'>
+                    <Scene key='Home' component={Home} initial={true} />
+                </Scene>
+            )}
+
+
+        </Router>
     )
 }
+
+export default Routes;
+
